535c443bcafde507cd82e5bb87bd7047aded3e32
RCB500X <ruben.cidb@icloud.com>
2021-07-28 23:53:52 -0400

    working

--- random_words/views.py
+++ random_words/views.py
@@ -3,15 +3,27 @@ from django.shortcuts import render, HttpResponse, redirect
 from django.http import JsonResponse
 from time import gmtime, strftime, localtime
 from datetime import datetime
-#from djanjo.utils import get_random_string
+from django.utils.crypto import get_random_string
 
 
 def random_page(request):
-    return render(request, "index.html")
+    counter = ()
+    if not counter:
+        counter = 0
+    context = {
+        'randomword': get_random_string(length=14),
+        'counter': counter
+    }
+    counter = counter + 1
+    return render(request, "index.html", context)
 
-#def random(request):
-#    if 'counter' no existe:
-#    'counter'++
-#        'counter' = 0
-#    context = {'randomword': get_random_string(length=14),
-#        'counter': 'counter'
+def random(request):
+    counter= 0
+    context = {
+        'randomword': get_random_string(length=14),
+        'counter': counter
+    }
+
+def session_info(request):
+    request.session['name'] = request.POST['name']
+    request.session['counter'] = 100
